851e16243d425cf9159e73b08f1432ac12f6d0f8
Breeze <60870327+QingFengM@users.noreply.github.com>
2025-05-06 15:47:01 +0800

    Update 阻止B站检测后台标签页运行.user.js

--- 阻止B站检测后台标签页运行.user.js
+++ 阻止B站检测后台标签页运行.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name         阻止B站检测后台标签页运行
 // @namespace    http://tampermonkey.net/
+// @homepage     https://github.com/QingFengM/Scripts/
 // @version      0.1
-// @description  Prevent websites from detecting background tabs
+// @description  阻止B站检测网页未处于当前页面运行时偷偷自动更改画质导致直播画面声音中断重连频繁卡顿
 // @match        https://live.bilibili.com/*
 // @grant        none
 // ==/UserScript==
