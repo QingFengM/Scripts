d84bd499a81a6f47be4c9a846a9a53ff30131a8f
Breeze <60870327+QingFengM@users.noreply.github.com>
2025-06-01 23:35:55 +0800

    Update 哔哩哔哩直播精简-原版界面.user.js

--- 哔哩哔哩直播精简-原版界面.user.js
+++ 哔哩哔哩直播精简-原版界面.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name                                                          哔哩哔哩直播精简-1.0
+// @name                                                          哔哩哔哩直播精简-原版界面
 // @description                                                   提供简洁的界面，只为安心看直播。
 // @homepage                                                      https://github.com/QingFengM/Scripts/
 // @author                                                        清风醉梦
