7e357b319d820d49a0c067f76bad2951452c8105
Breeze <60870327+QingFengM@users.noreply.github.com>
2026-08-18 15:33:49 +0800

    Update 哔哩哔哩播放页修改 （YouTube 布局）.user.js

--- bilibili/哔哩哔哩播放页修改 （YouTube 布局）.user.js
+++ bilibili/哔哩哔哩播放页修改 （YouTube 布局）.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         哔哩哔哩播放页修改 （YouTube 布局）
 // @namespace    http://tampermonkey.net/
-// @version      0.3.4.5
+// @version      0.3.4.6
 // @description  播放页主题色修改为#FB7299；扩展播放器宽高尺寸，优化适配页面布局；隐藏导航栏冗余入口、广告横幅、弹幕投票等干扰元素，提升观看体验与视觉整洁度。
 // @author       deepseek
 // @icon         https://www.bilibili.com/favicon.ico
@@ -340,6 +340,11 @@
         color: #FB7299 !important;
     }
 
+    /* 播放器阴影 */
+    .bpx-player-container {
+        box-shadow: 0 0 8px #CFCFCF !important;
+    }
+
     /* 播放器圆角 */
     .bpx-player-video-area,
     .bpx-player-container {
@@ -485,13 +490,6 @@
         background: transparent;
         color: #FB7299 !important;
     }
-
-
-
-
-
-
-
 
     /* 简介 */
     .video-desc-container .basic-desc-info a[data-v-4c8ea99d] {
